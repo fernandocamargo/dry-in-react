0d6f92ff948905ca50c67086ea959505cffc42a0
Fernando Camargo <fernando.camargo@sellics.com>
2019-04-02 01:03:34 +0200

    Add new example

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import GenericButton from "components/GenericButton";
 import Button from "components/Button";
 import ClickMe from "components/ClickMe";
 import CloseModal from "components/CloseModal";
@@ -7,6 +8,9 @@ import Image from "components/Image";
 
 export default () => (
   <div>
+    <GenericButton onClick={() => window.alert("nonCustomized();")}>
+      Simple as possible
+    </GenericButton>
     <Button />
     <ClickMe />
     <CloseModal active={false} />
